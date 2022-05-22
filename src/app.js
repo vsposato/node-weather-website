f8d158be60095b6c75a69f067bc3035c8c9e1298
Vincent Sposato <Vincent.Sposato@madmobile.com>
2022-05-22 12:33:14 -0400

    Tweaking help and about

--- src/app.js
+++ src/app.js
@@ -28,14 +28,14 @@ app.get('', (req, res) => {
 
 app.get('/about', (req, res) => {
   res.render('about', {
-    title: 'About Weather App',
+    title: 'About Me',
     name: 'Vincent Sposato'
   });
 });
 
 app.get('/help', (req, res) => {
   res.render('help', {
-    title: 'Help Weather App',
+    title: 'Help',
     name: 'Vincent Sposato',
     helpMessage: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
   });
